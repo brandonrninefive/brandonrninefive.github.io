6e918c93605c0605ff29432e009fc3b8d27c8b2f
Brandon Ruggles <brandon@localhost.localdomain>
2016-07-18 00:53:07 -0400

    Fixed some design errors

--- js/mainScript.js
+++ js/mainScript.js
@@ -7,15 +7,18 @@ function beginP1FadeIn()
       $("#thanksTitle").fadeIn(1200, function()
       {
         $("#buttonDiv").css("display","block");
-        $("#twitterButton").fadeTo(400, 1, function()
+        $("#twtterButton").fadeTo(400, 1, function()
         {
           $("#githubButton").fadeTo(400, 1, function()
           {
-            $("#linkedinButton").fadeTo(400, 1, function()
+            $("#lnkdnButton").fadeTo(400, 1, function()
             {
               $("#resumeButton").fadeTo(400, 1, function()
               {
-                $("#mainContainer2").fadeIn(800);
+                $("#emailP").fadeIn(400, function()
+                {
+                  $("#mainContainer2").fadeIn(800);
+                });
               });
             });
           });
@@ -28,44 +31,4 @@ function beginP1FadeIn()
 $(document).ready(function()
 {
   beginP1FadeIn();
-});
-
-$("#twitterButton").hover(function()
-{
-  $("#twitterButton").css("color","#80cbc4");
-});
-
-$("#twitterButton").mouseleave(function()
-{
-  $("#twitterButton").css("color","#26a69a");
-});
-
-$("#githubButton").hover(function()
-{
-  $("#githubButton").css("color","#80cbc4");
-});
-
-$("#githubButton").mouseleave(function()
-{
-  $("#githubButton").css("color","#26a69a");
-});
-
-$("#linkedinButton").hover(function()
-{
-  $("#linkedinButton").css("color","#80cbc4");
-});
-
-$("#linkedinButton").mouseleave(function()
-{
-  $("#linkedinButton").css("color","#26a69a");
-});
-
-$("#resumeButton").hover(function()
-{
-  $("#resumeButton").css("color","#80cbc4");
-});
-
-$("#resumeButton").mouseleave(function()
-{
-  $("#resumeButton").css("color","#26a69a");
 });
